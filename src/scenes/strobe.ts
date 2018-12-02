29f470280c636d10fba4c8201088c790489b9dff
Eric Rabil <ericjrabil@gmail.com>
2018-12-02 14:29:48 -0500

    Add strobe

---
+++ src/scenes/strobe.ts
@@ -0,0 +1,47 @@
+import { Scene, BaseSceneOptions } from "../struct/scene";
+import { HueApi, lightState } from "node-hue-api";
+import { randomNumber, RGB } from "../util/Colors";
+
+export interface StrobeOptions extends BaseSceneOptions {
+    activeColorGenerator?: () => RGB
+    inactiveColorGenerator?: () => RGB
+}
+
+export const BASE_STATE = lightState.create().turnOn().brightness(0).xy(...new RGB(255, 255, 255).xy).transitionInstant();
+
+export default class StrobeScene extends Scene<StrobeOptions> {
+    private lights: string[];
+    private currentLight: string | undefined;
+
+    constructor(api: HueApi, options: StrobeOptions) {
+        super(api, options, options.transition);
+    }
+
+    async init() {
+        const lightTargets = this.options.lights || [];
+        const groupTargets = this.options.groups || [];
+
+        const groups = await Promise.all(groupTargets.map(g => this.api.getGroup(g)));
+
+        this.lights = lightTargets.concat(
+            groups.map(group => group.lights || []).reduce((arr, lights) => arr.concat(lights), []));
+
+        await this.dispatch(BASE_STATE);
+    }
+
+    async next() {
+        await Promise.all([this.dispatch(this.nextBaseState, [], this.currentLight ? [this.currentLight] : []), this.dispatch(this.nextState, [], [this.nextLight])]);
+    }
+
+    private get nextState() {
+        return lightState.create().brightness(100).transitionInstant().xy(...(this.options.activeColorGenerator ? this.options.activeColorGenerator() : new RGB(255, 255, 255)).xy);
+    }
+
+    private get nextBaseState() {
+        return lightState.create().turnOn().brightness(0).xy(...(this.options.inactiveColorGenerator ? this.options.inactiveColorGenerator() : new RGB(255, 255, 255)).xy).transitionInstant();
+    }
+
+    private get nextLight(): string {
+        return this.currentLight = this.lights[randomNumber([0, this.lights.length - 1]).toFixed(0) * 1];
+    }
+}
