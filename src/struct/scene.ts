d18f3f0352296185688c367719e1d7181e080af7
Eric Rabil <ericjrabil@gmail.com>
2018-12-19 19:55:52 -0500

    Trim down scene processing

--- src/struct/scene.ts
+++ src/struct/scene.ts
@@ -2,6 +2,7 @@ import { HueApi, ILight, ILightGroup, lightState as LightState } from "node-hue-
 import logger from "../util/logging";
 import { sleep } from "../util/timing";
 import Target from "./target";
+import { State, setState } from "../util/state";
 
 export interface BaseSceneOptions extends Target {
     transition: number;
@@ -48,31 +49,36 @@ export abstract class Scene<T extends BaseSceneOptions = BaseSceneOptions> {
 
     protected async dispatch(state: LightState.State, groupsOverride?: string[], lightsOverride?: string[]) {
         if (this.stopped) {
-            logger.debug('dispatch called but scene is stopped. blocking.');
             return;
         }
         if (this.dropoutCount >= 10) {
-            logger.debug('scene keeps dropping out. cooling down.');
-            await sleep(5000);
-            logger.debug('recovery cooldown done. resuming.');
-            this.dropoutCount = 0;
+            await this.cooldown(5000);
         }
-        let {groups, lights} = this.options;
-        groups = groupsOverride || groups || [];
-        lights = lightsOverride || lights || [];
-        const startTime = Date.now();
-        await Promise.all(groups.map(group =>
-            this.api.setGroupLightState(typeof group === "string" ? group : group.id, state))
-                .concat(lights.map(light => this.api.setLightState(typeof light === "string" ? light : light.uniqueid, state)))
-        ).catch(e => {logger.warn('scene dropped a dispatch. dropouts: %s/10', this.dropoutCount + 1); this.dropoutCount++;});
-        const endTime = Date.now();
-        const elapsed = endTime - startTime;
+        const stateJob: State = {
+            target: {
+                groups: groupsOverride || this.groupTargetIDs,
+                lights: lightsOverride || this.lightTargetIDs
+            },
+            state
+        };
+        const elapsed = await setState(this.api, stateJob).catch(() => logger.warn('scene dropped a dispatch. dropouts: %s/10', this.dropoutCount++));
+        this.latestMetric = (elapsed as number) || 0;
+        logger.debug(`scene dispatched in ${elapsed}ms, average dispatch ${this.average.toFixed(2)}ms`);
+    }
+
+    private set latestMetric(value: number) {
         let averageSrc = this.average * this.averageCount;
         this.averageCount++;
-        averageSrc += elapsed;
+        averageSrc += value;
         averageSrc /= this.averageCount;
         this.average = averageSrc;
-        logger.debug(`scene dispatched in ${elapsed}ms, average dispatch ${averageSrc.toFixed(2)}ms`);
+    }
+
+    private async cooldown(time: number) {
+        logger.debug('scene keeps dropping out. cooling down for %sms.', time);
+        await sleep(time);
+        logger.debug('recovery cooldown done. resuming.');
+        this.dropoutCount = 0;
     }
     
     private delayedNext(loop: boolean = false) {
@@ -85,10 +91,10 @@ export abstract class Scene<T extends BaseSceneOptions = BaseSceneOptions> {
     protected abstract next(): Promise<void>;
 
     protected get lightTargetIDs() {
-        return (this.options.lights || []).map(l => typeof l === "string" ? l : l.uniqueid);
+        return (this.options.lights || []).map(l => typeof l === "object" ? l.uniqueid : l);
     }
 
     protected get groupTargetIDs() {
-        return (this.options.groups || []).map(g => typeof g === "string" ? g : g.id);
+        return (this.options.groups || []).map(g => typeof g === "object" ? g.id : g);
     }
 }
--- src/util/state.ts
+++ src/util/state.ts
@@ -7,11 +7,12 @@ export interface State {
 }
 
 export function setState(api: HueApi, state: State) {
+    const start = Date.now();
     return Promise.all(
-        (state.target.lights || []).map(light => api.setLightState(typeof light === "string" ? light : light.uniqueid, state)).concat(
-            (state.target.groups || []).map(group => api.setGroupLightState(typeof group === "string" ? group : group.id, state))
+        (state.target.lights || []).map(light => api.setLightState(typeof light === "object" ? light.uniqueid : light, state.state)).concat(
+            (state.target.groups || []).map(group => api.setGroupLightState(typeof group === "object" ? group.id : group, state.state))
         )
-    );
+    ).then(() => Date.now() - start);
 }
 
 export function setBulkState(api: HueApi, states: State[]) {
